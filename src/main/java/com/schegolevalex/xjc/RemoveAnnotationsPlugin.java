f83f6bdd08a8ec57ff13da5f18cf1c5bdaad54f4
schegolevalex <schegolevalex@gmail.com>
2023-09-11 20:48:32 +0300

    all annotations removed now

--- src/main/java/com/schegolevalex/xjc/RemoveAnnotationsPlugin.java
+++ src/main/java/com/schegolevalex/xjc/RemoveAnnotationsPlugin.java
@@ -1,10 +1,16 @@
 package com.schegolevalex.xjc;
 
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JFieldVar;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.Plugin;
+import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.Outline;
 import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class RemoveAnnotationsPlugin extends Plugin {
     public String getOptionName() {
@@ -15,15 +21,23 @@ public class RemoveAnnotationsPlugin extends Plugin {
         return "  -XRemoveAnnotationsPlugin    :  xjc plugin for remove all annotations from generated jaxb files";
     }
 
-    public boolean run(Outline outline, Options options, ErrorHandler errorHandler) throws SAXException {
+    public boolean run(Outline model, Options options, ErrorHandler errorHandler) {
+        for (ClassOutline classOutline : model.getClasses()) {
+            Collection<JAnnotationUse> removedClassAnnotations = classOutline.getImplClass().annotations();
+            List<JAnnotationUse> clonedRemovedClassAnnotations = new ArrayList<>(removedClassAnnotations);
+            for (JAnnotationUse removedAnnotation : clonedRemovedClassAnnotations) {
+                classOutline.getImplClass().removeAnnotation(removedAnnotation);
+            }
 
-
-
-
-
-
-
-
-        return false;
+            Collection<JFieldVar> fields = classOutline.getImplClass().fields().values();
+            for (JFieldVar jFieldVar : fields) {
+                Collection<JAnnotationUse> removedFieldAnnotations = jFieldVar.annotations();
+                ArrayList<JAnnotationUse> clonedRemovedFieldAnnotations = new ArrayList<>(removedFieldAnnotations);
+                for (JAnnotationUse removedFieldAnnotation : clonedRemovedFieldAnnotations) {
+                    jFieldVar.removeAnnotation(removedFieldAnnotation);
+                }
+            }
+        }
+        return true;
     }
 }
